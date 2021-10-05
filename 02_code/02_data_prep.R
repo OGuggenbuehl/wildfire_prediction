54ea9dead045d4b97fb9015623039a65c5e0b81d
Oliver Guggenbühl <oliver.guggenbuehl@statworx.com>
2021-10-05 17:48:38 +0200

    minor

--- 02_code/02_data_prep.R
+++ 02_code/02_data_prep.R
@@ -1,1 +1,14 @@
 library(tidyverse)
+
+# load data ---------------------------------------------------------------
+data <- read_rds("01_data/data_final.rds")
+
+# data exploration --------------------------------------------------------
+str(data)
+
+# data preparation --------------------------------------------------------
+
+dummies <- c("river", "lake", "recreational_routes", "campground",
+             "state_park", "picnic", "powerline", "road")
+
+distance <- c("dist_lake", "dist_power", "dist_river", "dis_road")
