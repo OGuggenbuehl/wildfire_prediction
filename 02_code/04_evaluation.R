a35f9782c27ef93e4dc7cb551bd9f2cd99ddf1a6
Oliver Guggenbühl <oliver.guggenbuehl@statworx.com>
2021-11-17 01:04:35 +0100

    compile metrics table

--- 02_code/04_evaluation.R
+++ 02_code/04_evaluation.R
@@ -40,7 +40,7 @@ source("02_code/04.9_XGB_tune.R", echo = TRUE)
 # Stacking ----------------------------------------------------------------
 source("02_code/04.10_stacking.R", echo = TRUE)
 
-glm_res_up_metrics %>% 
+model_comp <- glm_res_up_metrics %>% 
   bind_rows(glm_res_down_metrics) %>% 
   bind_rows(glm_tuned_up_metrics) %>% 
   bind_rows(glm_tuned_down_metrics) %>% 
@@ -52,10 +52,33 @@ glm_res_up_metrics %>%
   bind_rows(xgb_res_down_metrics) %>% 
   bind_rows(xgb_tuned_up_metrics) %>% 
   bind_rows(xgb_tuned_down_metrics) %>% 
-  mutate(.estimate = if_else(is.na(.estimate), true = mean, false = .estimate)) %>% 
+  mutate(.estimate = if_else(is.na(.estimate), 
+                             true = mean, 
+                             false = .estimate)) %>% 
   select(.metric, .estimate, model) %>% 
   bind_rows(glm_naive_metrics) %>% 
   bind_rows(rf_naive_metrics) %>% 
   bind_rows(xgb_naive_metrics) %>% 
   filter(.metric %in% my_metrics) %>% 
-  arrange(model, .metric) %>% View()
+  arrange(model, .metric) %>% 
+  pivot_wider(names_from = model, values_from = .estimate)
+
+models_naive <- model_comp %>% 
+  select(.metric, dplyr::contains("naive"))
+models_res <- model_comp %>% 
+  select(.metric, dplyr::contains("res"))
+models_tuned <- model_comp %>% 
+  select(.metric, dplyr::contains("tune"))
+
+model_comp_list <- list('naive' = models_naive,
+                        'resampled' = models_res,
+                        'tuned' = models_tuned)
+
+write_rds(model_comp_list, "03_outputs/model_comp.rds")
+
+write.table(model_comp_list$naive, file = "03_outputs/model_comp_naive.txt", 
+            sep = ",", quote = FALSE, row.names = FALSE)
+write.table(model_comp_list$resampled %>% View(), file = "03_outputs/model_comp_res.txt", 
+            sep = ",", quote = FALSE, row.names = FALSE)
+write.table(model_comp_list$tuned, file = "03_outputs/model_comp_tuned.txt", 
+            sep = ",", quote = FALSE, row.names = FALSE)
